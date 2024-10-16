1c5c4a5f374e031752246bf3dfd85df6c30fe530
Patryk Wozniczka <patryk@archi-lab.net>
2024-10-16 19:04:59 -0400

    Add ControlNet Pipeline

--- model-server/main.py
+++ model-server/main.py
@@ -2,12 +2,12 @@
 import io
 from typing import Union
 from pathlib import Path
-from PIL import Image
+from PIL import Image, ImageOps
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse, StreamingResponse
 
-from ml import obtain_image
+from ml import text_to_image, obtain_image, obtain_control_net_image
 
 app = FastAPI()
 
@@ -40,13 +40,23 @@ async def generate_image(
 
         image.save("image.png")
         return FileResponse("image.png")
+    
+@app.post("/text-to-image")
+async def generate_image_from_text(prompt: str = "Astronaut riding a horse"):
+    image = text_to_image(prompt)
+    memory_stream = io.BytesIO()
+    image.save(memory_stream, format="PNG")
+    memory_stream.seek(0)
+    return StreamingResponse(memory_stream, media_type="image/png")
 
 @app.post("/generate-memory")
 async def generate_image_memory(
     prompt: str =  "a pixar style, highfidelity image of a building in the middle of a forest",
     file: UploadFile | None = File(None),
+    num_inference_steps: int = 50,
     guidance_scale: float = 7.5,
     strength: float = 0.85,
+    seed: int = None,
 ):
     if not file:
         raise ValueError("No file uploaded")
@@ -65,13 +75,63 @@ async def generate_image_memory(
             init_image=init_image,
             guidance_scale=guidance_scale,
             strength=strength,
-            num_inference_steps=10,
+            num_inference_steps=num_inference_steps,
+            seed=seed,
         )
 
         memory_stream = io.BytesIO()
         image.save(memory_stream, format="PNG")
         memory_stream.seek(0)
         return StreamingResponse(memory_stream, media_type="image/png")
+
+@app.post("/generate-controlnet")
+async def generate_control_net_image(
+    prompt: str = "aerial view, a futuristic house in a bright foggy jungle, hard lighting",
+    negative_prompt: str = "low quality, bad quality, sketches",
+    file: UploadFile = File(...),
+    num_inference_steps: int = 50,
+    guidance_scale: float = 5.0,
+    controlnet_conditioning_scale: float = 0.5,
+    seed: int = None,
+):
+    if not file:
+        raise ValueError("No file uploaded")
+    else:
+        if file.content_type not in ["image/jpeg", "image/png"]:
+            raise ValueError("File format not supported. Please upload a .jpg or .png image.")
+
+    image_data = await file.read()
+    init_image = Image.open(io.BytesIO(image_data)).convert("RGB")
+    init_image = crop_and_resize_image(init_image)
+
+    image = obtain_control_net_image(
+        prompt,
+        negative_prompt,
+        init_image=init_image,
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
+        controlnet_conditioning_scale=controlnet_conditioning_scale,
+        seed=seed,
+    )
+
+    memory_stream = io.BytesIO()
+    image.save(memory_stream, format="PNG")
+    memory_stream.seek(0)
+    return StreamingResponse(memory_stream, media_type="image/png")
+
+@app.post("/invert-rgb")
+async def invert_rgb(file: UploadFile = File(...)):
+    if file.content_type not in ["image/jpeg", "image/png"]:
+        raise ValueError("File format not supported. Please upload a .jpg or .png image.")
+
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data)).convert("RGB")
+    image = crop_and_resize_image(image)
+    image = ImageOps.invert(image)
+    memory_stream = io.BytesIO()
+    image.save(memory_stream, format="PNG")
+    memory_stream.seek(0)
+    return StreamingResponse(memory_stream, media_type="image/png")
     
 
 def crop_and_resize_image(image: Image.Image, size: tuple[int, int] = (1024, 1024)) -> Image.Image:
--- model-server/ml.py
+++ model-server/ml.py
@@ -1,9 +1,16 @@
 import torch
-from diffusers import StableDiffusionXLImg2ImgPipeline
+from diffusers import (
+    ControlNetModel,
+    StableDiffusionXLPipeline,
+    StableDiffusionXLImg2ImgPipeline,
+    StableDiffusionXLControlNetPipeline,
+    AutoencoderKL)
 from diffusers.utils import load_image
 from PIL import Image
+import numpy as np
+import cv2
 
-# model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+model_id_text = "stabilityai/stable-diffusion-xl-base-1.0"
 model_id = "stabilityai/stable-diffusion-xl-refiner-1.0"
 
 pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
@@ -17,24 +24,80 @@ pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
 # pipe.enable_model_cpu_offload()
 pipe = pipe.to("cuda")
 
+pipe_text_to_image = StableDiffusionXLPipeline.from_pretrained(
+    model_id_text,
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True
+)
+pipe_text_to_image = pipe_text_to_image.to("cuda")
+
+def text_to_image(prompt: str) -> Image:
+    image: Image = pipe_text_to_image(prompt=prompt).images[0]
+    return image
+
 def obtain_image(
     prompt: str,
     init_image: Image,
     num_inference_steps: int = 50,
     strength: float = 0.85,
     guidance_scale: float = 7.5,
+    seed: int = None
 ) -> Image:
     # generator to make generation deterministic/reproducible
-    # generator = None if seed is None else torch.Generator("cuda").manual_seed(seed)
+    generator = None if seed is None else torch.Generator("cuda").manual_seed(seed)
     print(f"Using device: {pipe.device}")
     print(f"Guidance scale: {guidance_scale}")
     print(f"Strength: {strength}")
+    print(f"Seed: {seed}")
     image: Image = pipe(
         prompt,
         image=init_image,
         strength=strength,
         num_inference_steps=num_inference_steps,
         guidance_scale=guidance_scale,
+        generator=generator
         
+    ).images[0]
+    return image
+
+control_net_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+controlnet = ControlNetModel.from_pretrained(
+    "diffusers/controlnet-canny-sdxl-1.0",
+    torch_dtype=torch.float16
+).to("cuda")
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
+pipe_controlnet = StableDiffusionXLControlNetPipeline.from_pretrained(
+    control_net_model_id,
+    controlnet=controlnet,
+    vae=vae,
+    torch_dtype=torch.float16,
+)
+pipe_controlnet = pipe_controlnet.to("cuda")
+# pipe_controlnet.enable_model_cpu_offload()
+
+
+def obtain_control_net_image(
+        prompt: str,
+        negative_prompt: str,
+        init_image: Image,
+        num_inference_steps: int = 50,
+        guidance_scale: float = 5.0,
+        controlnet_conditioning_scale: float = 0.5,
+        seed: int = None
+
+) -> Image:
+    generator = None if seed is None else torch.Generator("cuda").manual_seed(seed)
+    print(f"Using device: {pipe_controlnet.device}")
+    print(f"ControlNet conditioning scale: {controlnet_conditioning_scale}")
+
+    image: Image = pipe_controlnet(
+        prompt,
+        negative_prompt=negative_prompt,
+        image=init_image,
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
+        controlnet_conditioning_scale=controlnet_conditioning_scale,
+        generator=generator
     ).images[0]
     return image
